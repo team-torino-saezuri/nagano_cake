d3030d51c5f5e5909dc074a94bcd8d71eae9fd3c
mizuhoiimori <tanaka.mizuho.com@gmail.com>
2021-11-16 09:14:07 +0000

    admin:itemを作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  has_many :cart_items, dependent: :destroy
+  has_many :order_details, dependent: :destroy
+  has_many :orders, through: :order_details
+  
+  belongs_to :genre
+  
+  attachment :image
+  
+  validates :name,presence: true
+  validates :image, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
+  validates :genre_id, presence: true
 end
