fe1d4e988111a539d3056d8f046dca7dbf534613
takaki-0611 <pq.earlyripe.earlyrotten@gmail.com>
2021-11-16 12:40:30 +0000

    confirm編集

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,6 @@ class Order < ApplicationRecord
     presence: true
   validates :postal_code, length: {is: 7}, numericality: { only_integer: true }
   validates :shipping_cost, :total_payment, numericality: { only_integer: true }
+  enum payment_method: {"クレジットカード": 0,"銀行振込み": 1}
+  enum status: {"入金待ち": 0,"入金確認": 1,"製作中": 2,"発送準備中": 3,"発送済み": 4}
 end
